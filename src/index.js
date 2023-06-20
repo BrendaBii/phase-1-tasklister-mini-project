6594a1afc4b81d620ee20d6cf3627574a10ee739
BrendaBii <bii.brenda.bcb@gmail.com>
2023-06-20 18:23:28 +0300

    Completed assignment.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", addToDo);
+
+  function addToDo(e){
+    e.preventDefault();
+    let entry = document.getElementById("new-task-description").value;
+    let li = document.createElement("li");
+    let del = document.createElement("button");
+    del.textContent = "x";
+    del.addEventListener("click", () => del.parentNode.remove());
+    let ul = document.getElementById("tasks");
+    li.textContent = entry + "  ";
+    ul.appendChild(li);
+    li.appendChild(del);
+    form.reset();
+  }
 });
